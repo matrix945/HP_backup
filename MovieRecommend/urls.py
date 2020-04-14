960eb00964294f3da42caccf0f028e24da4be68a
matrix945 <frankzhang945@gmail.com>
2020-04-14 04:14:29 -0400

    change name -> Recommend add back a file

--- MovieRecommend/urls.py
+++ MovieRecommend/urls.py
@@ -1,4 +1,4 @@
-"""MovieHunter URL Configuration
+"""MovieRecommend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- MovieRecommend/wsgi.py
+++ MovieRecommend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MovieHunter project.
+WSGI config for MovieRecommend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MovieHunter.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MovieRecommend.settings")
 
 application = get_wsgi_application()
